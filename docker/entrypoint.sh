57ad507c3001c3ba8eef01cb40e1e7035dd6fed3
Craig Burdulis <craigburdulis@CJ-LT.local>
2020-01-20 20:52:25 -0500

    Added check for ENVIRON file before proceeding

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,13 @@
 
 # Check for ENVIRON environment variables
 if [[ ! -v ENVIRON ]]; then
-  echo "Environment variable `ENVIRON` was not provided. Please provide value before starting container." 1>&2
+  echo "Environment variable ENVIRON was not provided. Please provide value before starting container." 1>&2
+  exit 1
+fi
+
+# Check for ENVIRON's env file
+if [[ ! -f "/bootstrap/$ENVIRON" ]]; then
+  echo "No environment variable file provided for the current environment. Please make sure your ENVIRON value is valid." 1>&2
   exit 1
 fi
 
